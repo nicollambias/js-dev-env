39e9b8bd56f29be3b6c330e45a8a95afa72067a1
Nicolas Llambías <nicollambias@gmail.com>
2018-10-25 00:59:07 -0300

    primer commit

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,4 @@
+import { green } from 'chalk';
+
+console.log(green('Starting app in dev mode...')); // eslint-disable-line no-console
+
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './index.css';
+import numeral from 'numeral';
+
+const courseValue = numeral(1000).format('$0,00.0');
+console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
